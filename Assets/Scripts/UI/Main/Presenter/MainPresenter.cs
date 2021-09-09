ffe37aa6b11dfe7ce1abb84fcbefbf3860ad946e
kozaimikuto <c612101286@tokushima-u.ac.jp>
2021-09-10 01:49:09 +0900

    テキストを変更できるようにしました。（おそらく）

--- Assets/Scripts/UI/Main/Presenter/MainPresenter.cs
+++ Assets/Scripts/UI/Main/Presenter/MainPresenter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 using Patient;
+using UnityEngine.UI;
 
 namespace UI.Main.Presenter
 {
@@ -14,20 +15,24 @@ namespace UI.Main.Presenter
         [SerializeField]
         private PatientCountManager countManager;
 
+        public Text text;
+
         private void Start()
         {
             patientSpawn.CurrentWave
                 .SkipLatestValueOnSubscribe()
-                .Subscribe(waveCount =>
+                .Subscribe(waveCount => 
                 {
                     // wave をすべて攻略した時
                     if (waveCount == 4)
                     {
-
+                        text.text = "ウェーブがall終わった（仮）";
+                        Debug.Log("特になし");
                     }
                     // wave が始まった時
                     else
                     {
+                        text.text = "ウェーブが始まった（仮）";
                     }
                 }).AddTo(this);
 
@@ -35,8 +40,10 @@ namespace UI.Main.Presenter
             countManager.CurrentPatientCount
                 .SkipLatestValueOnSubscribe()
                 .Where(count => count <= 0)
-                .Subscribe(count =>
+                .Subscribe(count => 
                 {
+                    text.text = "患者の殲滅に成功した！（仮）";
+                    Debug.Log("一応文章入れておこうかなと思うけれど、これって結局意味ないのではと考えた。でもそれは今こんなことを考えながらタイプしている自分が悪いのだ。");
                 }).AddTo(this);
         }
     }
